6d6e1e876f18eb70a3fd3a98694bd101f364c101
kristyyip <59596642+kristyyip@users.noreply.github.com>
2020-01-15 17:46:17 -0500

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,16 @@
+
+
+# some english message
+print("Hello World")
+
+# doing something else
+
+# adapted from [link]
+
+print("Hello World", "Some other message")
+
+x = 5
+
+print(x)
+
+print(x + 100)
